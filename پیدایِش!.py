302742cbc71c0d1323ba586f965272662df1ed88
Hossein Valizadeh <hosseinvalizadeh1384@gmail.com>
2026-07-03 03:12:19 +0330

    Update پیدایِش!.py

--- پیدایِش!.py
+++ پیدایِش!.py
@@ -1,6 +1,5 @@
 # In this question, we should find a number that doesn't exist in list of numbers from 1 to 4.
 # We also get three numbers from function to find the non-exist number!
-
 def find(num1, num2, num3):
     all_numbers = [1, 2, 3, 4]
     existed_numbers = [num1, num2, num3]
